0a8ab03cae1b909745fbc341fcf0fc3ea441ecc5
sergiod <sergiod@dos.sergio>
2024-07-20 16:55:12 -0600

    ci: add env variables

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
-import { createPool } from 'mysql2/promise';
+import { createPool } from "mysql2/promise";
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from "./config.js";
 
 export const pool = createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '2109207',
-  database: 'task4db'
-})
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
