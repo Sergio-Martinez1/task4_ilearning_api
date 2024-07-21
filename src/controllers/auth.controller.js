4a275b109ac29fd78e89cd3665856f2d25063de8
sergiod <sergiod@dos.sergio>
2024-07-21 10:04:31 -0600

    fix(cookies): adjust login cookie to samesite none

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,7 +45,12 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Incorrect password" });
 
     const token = await createAccesToken({ id: user.id });
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    });
 
     let currentDateTime = new Date();
 
@@ -76,6 +81,7 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
+    console.log(error)
   }
 };
 
