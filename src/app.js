5b06a424432ff3fcf0aa411cc48183c8ad43cdd7
sergiod <sergiod@dos.sergio>
2024-07-20 20:10:13 -0600

    refactor: update frontend url

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task4ilearningclient-production.up.railway.app",
     credentials: true,
   }),
 );
