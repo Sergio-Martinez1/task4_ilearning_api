f06ec9d2a8c930d5f590a1ca474aea909da24a42
sergiod <sergiod@dos.sergio>
2024-07-21 23:37:38 -0600

    fix(error): add handling for duplicate key

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,7 +22,11 @@ export const signUp = async (req, res) => {
 
     res.status(200).json({ message: "User created" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    if (error.code === "ER_DUP_ENTRY") {
+      res.status(400).json({ message: "This email already exists" });
+    } else {
+      res.status(500).json({ message: "There was an error in user sigup" });
+    }
   }
 };
 
